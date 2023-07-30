7469a5f8ff620d17d7dfbb3584b0b596e61c577b
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-07-30 01:21:41 -0700

    java script

---
+++ array.js
@@ -0,0 +1,15 @@
+// // ARRAY METHODS
+ let num = [1,2,3,4,5]
+//  let a =num.toString()
+// //  a now a string 
+//  console.log(a,typeof a)
+//  let c = num.join
+//  consol.log(c, typeof c)
+// Number#2
+// // pop is used to return the last element of array? 
+// let r = num.pop()
+// //  console.log(r)
+//  0@Number#3
+// //  Appends new elements to the end of an array, and returns the new length of array?
+//  let  r = num.push(8,6,9)
+//  console.log(r)
