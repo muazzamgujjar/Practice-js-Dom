101747c9facb7e2367a557ad5338d033cb1d2bc4
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-10 07:25:13 -0700

    initial commit

---
+++ Attribute-Method/script.js
@@ -0,0 +1,13 @@
+// let first = document.getElementById("first")
+// let a = first.getAttribute("class")
+// console.log(a)
+// console.log(first.hasAttribute("class"))
+// console.log(first.hasAttribute("style"))
+// console.log(  first.setAttribute("hidden","tru"))
+// first.setAttribute("fall","tru")
+// first.removeAttribute("class")
+//  console.log(first.Attribute)
+  console.log(first.dataset)
+  console.log(first.dataset.game)
+  console.log(first.dataset.player)
+  
